27af2d0a280082296665fb2d44a3a6d129590d4d
Rinat Raimov <raimovrinat@yandex.ru>
2023-10-08 16:47:04 +0400

    добавил решение задачи №1 и задачи №2

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  let factorial = 1;
+
+  if (n > 1) {
+    for (let j = n; j > 1; j--) {
+      factorial *= j;
+    }
+    return factorial;
+  }
+  else {
+    return 1;
+  }
 }
+
+

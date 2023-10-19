7c04ade49e787adab6d6dbdc2b992e4e1142f796
Rinat Raimov <raimovrinat@yandex.ru>
2023-10-19 14:41:15 +0300

    Исправил ошибки в задаче №3 и №4

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,30 +1,39 @@
-function splitString(str) {
-  
-  let splittedString = str.split('-'); 
-  
-  return splittedString;
+ function camelize(str) {
+  if (!str) {
+    return str;
+  }
+
+  return str
+    .split('-') 
+    .map((item, index) => {
+      
+      if (index === 0) { // Это нужно делать, чтобы не делать заглавной первую часть
+        return item;
+      }
+
+      let firstLetter = item.slice(0, 1);
+      let rest = item.slice(1);
+
+      return `${firstLetter.toUpperCase()}${rest}`;
+    })
+    .join('');
 }
 
 
-function camelize(str) {
-  let splittedString = splitString(str);
+// function camelize(str) {
+//   let splittedString = str.split('-'); 
    
-  let camelizedArray = [];
+//   let camelizedArray = [];
   
-  for (let i = 1; i < splittedString.length; i++) {
-    camelizedArray[i] = splittedString[i][0].toUpperCase() + splittedString[i].slice(1);
-  }
+//   for (let i = 1; i < splittedString.length; i++) {
+//     camelizedArray[i] = splittedString[i][0].toUpperCase() + splittedString[i].slice(1);
+//   }
   
-  camelizedArray[0] = splittedString[0];
+//   camelizedArray[0] = splittedString[0];
   
-  let joinedArray = camelizedArray.join('');
+//   let joinedArray = camelizedArray.join('');
   
-  return joinedArray;
- }
-
-
-camelize("background-color");
-camelize("list-style-image");
-camelize('-webkit-transition');
+//   return joinedArray;
+//  }
 
 
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,27 +1,3 @@
-let users = [{
-  "balance": "$1,825.65",
-  "picture": "https://placehold.it/32x32",
-  "age": 21,
-  "name": "Golden Branch",
-  "gender": "male",
-  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-},
-{
-  "balance": "$1,490.15",
-  "picture": "https://placehold.it/32x32",
-  "age": 31,
-  "name": "Duncan Randall",
-  "gender": "male",
-  "greeting": "Hello, Duncan Randall! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-},            
-            
-];
-
-
-
-
 function showSalary(users, age) {
 
   if ( Array.isArray(users) && users.length > 0 && (typeof age) == 'number' ) {
@@ -48,5 +24,5 @@ function showSalary(users, age) {
   }
 }
 
-showSalary(users, 40);
+
 
